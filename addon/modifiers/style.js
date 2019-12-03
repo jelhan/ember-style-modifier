cc3fd6f9902956d23e6b2d61879e51d5352dc14a
Andrey Fel <andrey.fel@retailnext.net>
2019-12-03 18:28:18 +0300

    Rewrite the modifier based on ember-modifier
    ember-modifier will likely become a standard for modifiers.
    No need to implement our own modifier manager.

--- addon/modifiers/style.js
+++ addon/modifiers/style.js
@@ -1,90 +1,72 @@
-import { setModifierManager, capabilities } from '@ember/modifier';
+import Modifier from 'ember-modifier';
 import { dasherize } from '@ember/string';
 import { assert } from '@ember/debug';
 import { typeOf } from '@ember/utils';
 
-const isObject = o => typeof o === 'object' && Boolean(o);
-
-/**
- * Returns a two-dimensional array, like:
- *
- * ```js
- * [
- *   ['font-size', '16px'],
- *   ['text-align', 'center'],
- *   ['color', 'red']
- * ]
- * ```
- *
- * This data structure is slightly faster to process than an object / dictionary.
- */
-const getStylesFromOptions = (positional, named) =>
-  // This is a workaround for the missing `Array#flat` in IE11.
-  [].concat(
-    ...[...positional.filter(isObject), named].map(obj =>
-      Object.entries(obj).map(([k, v]) => [dasherize(k), v])
-    )
-  );
-
-function setStyles(element, newStyles, oldStyles) {
-  const rulesToRemove = oldStyles ? new Set(oldStyles.map(e => e[0])) : null;
-
-  newStyles.forEach(([property, value]) => {
-    assert(
-      `Your given value for property '${property}' is ${value} (${typeOf(value)}). ` +
-      'Accepted types are string and undefined. Please change accordingly.',
-      typeof value === 'undefined' || typeOf(value) === 'string'
-    );
-
-    // priority must be specified as separate argument
-    // value must not contain "!important"
-    let priority = '';
-    if (value && value.includes('!important')) {
-      priority = 'important';
-      value = value.replace('!important', '');
-    }
-
-    // support camelCase property name
-    property = dasherize(property);
-
-    element.style.setProperty(property, value, priority);
-
-    if (rulesToRemove) {
-      rulesToRemove.delete(property);
-    }
-  });
-
-  if (rulesToRemove) {
-    rulesToRemove.forEach(rule => element.style.removeProperty(rule));
-  }
+function isObject(o) {
+  return typeof o === 'object' && Boolean(o);
 }
 
-export default setModifierManager(
-  () => ({
-    capabilities: capabilities('3.13'),
+export default class StyleModifier extends Modifier {
+  /**
+   * Returns a two-dimensional array, like:
+   *
+   * ```js
+   * [
+   *   ['font-size', '16px'],
+   *   ['text-align', 'center'],
+   *   ['color', 'red']
+   * ]
+   * ```
+   *
+   * This data structure is slightly faster to process than an object / dictionary.
+   */
+  get styles() {
+    const { positional, named } = this.args;
+    // This is a workaround for the missing `Array#flat` in IE11.
+    return [].concat(
+      ...[...positional.filter(isObject), named].map(obj =>
+        Object.entries(obj).map(([k, v]) => [dasherize(k), v])
+      )
+    );
+  }
 
-    createModifier() {
-      return {
-        element: null,
-        styles: {}
-      };
-    },
+  setStyles(newStyles) {
+    const oldStyles = this._oldStyles || [];
+    this._oldStyles = newStyles;
+    const rulesToRemove = oldStyles ? new Set(oldStyles.map(e => e[0])) : null;
 
-    installModifier(state, element, { positional, named }) {
-      state.element = element;
-      state.styles = getStylesFromOptions(positional, named);
-      setStyles(element, state.styles);
-    },
+    newStyles.forEach(([property, value]) => {
+      assert(
+        `Your given value for property '${property}' is ${value} (${typeOf(value)}). ` +
+        'Accepted types are string and undefined. Please change accordingly.',
+        typeof value === 'undefined' || typeOf(value) === 'string'
+      );
 
-    updateModifier(state, { positional, named }) {
-      const newStyles = getStylesFromOptions(positional, named);
-      setStyles(state.element, newStyles, state.styles);
-      state.styles = newStyles;
-    },
+      // priority must be specified as separate argument
+      // value must not contain "!important"
+      let priority = '';
+      if (value && value.includes('!important')) {
+        priority = 'important';
+        value = value.replace('!important', '');
+      }
 
-    destroyModifier({ element, styles }) {
-      setStyles(element, [], styles);
+      // support camelCase property name
+      property = dasherize(property);
+
+      this.element.style.setProperty(property, value, priority);
+
+      if (rulesToRemove) {
+        rulesToRemove.delete(property);
+      }
+    });
+
+    if (rulesToRemove) {
+      rulesToRemove.forEach(rule => this.element.style.removeProperty(rule));
     }
-  }),
-  class StyleModifier {}
-);
+  }
+
+  didReceiveArguments() {
+    this.setStyles(this.styles);
+  }
+}
