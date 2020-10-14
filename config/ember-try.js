b54db93251bee2b8c6617d4fa59cf49f246f3262
Jeldrik Hanschke <admin@jhanschke.de>
2020-10-14 15:45:49 +0200

    upgrade ember-cli to 3.21 including blueprints

--- config/ember-try.js
+++ config/ember-try.js
@@ -39,6 +39,14 @@ module.exports = async function() {
         }
       },
       {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.20.5'
+          }
+        }
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
@@ -71,7 +79,7 @@ module.exports = async function() {
         },
         npm: {
           devDependencies: {
-            '@ember/jquery': '^0.5.1'
+            '@ember/jquery': '^1.1.0'
           }
         }
       },
