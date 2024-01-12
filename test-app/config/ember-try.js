4493469fdebf136180faa096387ee5f4e3a95121
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-01-12 16:12:13 +0100

    Add ember try for ember-source >= 3.28

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -8,10 +8,48 @@ module.exports = async function () {
     usePnpm: true,
     scenarios: [
       {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            '@ember/test-helpers': '^2.9.4',
+            '@types/ember-resolver': '^5.0.13',
+            'ember-cli': '~4.12.2',
+            'ember-qunit': '^6.0.0',
+            'ember-resolver': '^8.0.0',
+            'ember-source': '~3.28.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.4',
+        npm: {
+          devDependencies: {
+            'ember-resolver': '^8.0.0',
+            'ember-source': '~4.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
+          },
+        },
+      },
+      {
         name: 'ember-lts-4.12',
         npm: {
           devDependencies: {
             'ember-source': '~4.12.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-5.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.4.0',
           },
         },
       },
